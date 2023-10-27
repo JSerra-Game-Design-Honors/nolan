f44a503b5c33cf0ea98bf7417a58846e97f9198a
nolanbaker60 <143121705+nolanbaker60@users.noreply.github.com>
2023-10-27 10:46:47 -0700

    Final prototype

---
+++ Main project/Assets/AlterunaFPS/Scripts/UI/GameInitRead.cs
@@ -0,0 +1,31 @@
+﻿using Alteruna;
+using UnityEngine.Events;
+
+namespace AlterunaFPS
+{
+	public class GameInitRead : CommunicationBridge
+	{
+		public UnityEvent InitHost;
+		public UnityEvent InitHostConnect;
+		public UnityEvent InitClient;
+
+		private void Start()
+		{
+			if (GameInitSet.Host)
+			{
+				Multiplayer.OnConnected.AddListener(CallInitHost);
+				InitHost.Invoke();
+			}
+			else
+			{
+				InitClient.Invoke();
+			}
+		}
+
+		private void CallInitHost(Multiplayer arg0, Endpoint arg1)
+		{
+			arg0.OnConnected.RemoveListener(CallInitHost);
+			InitHostConnect.Invoke();
+		}
+	}
+}
